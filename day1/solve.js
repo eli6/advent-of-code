2343bc033719da1023b6d1a8fc689c45cb8ba447
Elin Fritiofsson <fritiofsson@gfai.de>
2021-12-01 09:48:27 +0100

    Failing testcase for readfile

--- day1/solve.js
+++ day1/solve.js
@@ -1,3 +1,8 @@
 module.exports.countIncreases = (dataFile) => {
     return 0;
 }
+
+
+module.exports.readData = (dataFile) => {
+    return new Array();
+}
--- day1/solve.test.js
+++ day1/solve.test.js
@@ -1,6 +1,11 @@
 const solve = require('./solve');
 const dataFile = "testdata.txt";
 
-test('counts number of increases in data series', () => {
+test('countIncreases counts number of increases in data series', () => {
   expect(solve.countIncreases(dataFile)).toBe(7);
+});
+
+
+test('readFile creates an array with 10 entries from the testfile', () => {
+    expect(solve.readData(dataFile).length).toBe(7);
 });
