c62161e6ee880c4a1fbd588d96eb51d5d45e6bfa
Elin Fritiofsson <fritiofsson@gfai.de>
2021-12-01 11:50:41 +0100

    Added code so that readdata test passes

--- day1/solve.js
+++ day1/solve.js
@@ -1,8 +1,33 @@
+var fs = require('fs');
+var readline = require('readline');
+const path = require('path');
+const { resolve } = require('path');
+
+
 module.exports.countIncreases = (dataFile) => {
     return 0;
 }
 
+module.exports.readData = async (dataFile) => {
 
-module.exports.readData = (dataFile) => {
-    return new Array();
+    return new Promise((resolve, reject) => {
+
+        const filePath = path.join(__dirname, dataFile);
+        let readLineInterface = readline.createInterface({
+            input: fs.createReadStream(filePath),
+            console: false
+        })
+
+        let inputList = new Array();
+        
+        readLineInterface.on("line", line => {
+            inputList.push(line.trim());
+            console.log(line);
+        }).on("error", error => {
+            console.log(error);
+            reject(new Array());
+        }).on("close", ()=>{
+            resolve(inputList);
+        })
+    });
 }
--- day1/solve.test.js
+++ day1/solve.test.js
@@ -1,11 +1,13 @@
 const solve = require('./solve');
-const dataFile = "testdata.txt";
+let dataFile = "testdata.txt";
 
 test('countIncreases counts number of increases in data series', () => {
   expect(solve.countIncreases(dataFile)).toBe(7);
 });
 
 
-test('readFile creates an array with 10 entries from the testfile', () => {
-    expect(solve.readData(dataFile).length).toBe(7);
+test('readFile creates an array with 10 entries from the testfile', async () => {
+
+    let solutionArray = await solve.readData(dataFile);
+    expect(solutionArray.length).toBe(10);
 });
