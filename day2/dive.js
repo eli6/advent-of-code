a8782eb1785ce0f6f1f937e0e50393a1a37a6103
Elin Fritiofsson <fritiofsson@gfai.de>
2021-12-02 09:16:38 +0100

    Passing test for depth as well

--- day2/dive.js
+++ day2/dive.js
@@ -15,5 +15,16 @@ module.exports.driveSubmarine = (movementData) => {
         return accumulator;
     }, 0);
 
+    position.depth = movementData.reduce((accumulator, currValue, currIndex, array)=> {
+        let currentDirectionArray = currValue.split(' ');
+        if(currentDirectionArray[0] === "up")
+            return accumulator-Number(currentDirectionArray[1]);
+
+        if(currentDirectionArray[0] === "down")
+            return accumulator+Number(currentDirectionArray[1]);
+
+        return accumulator;
+    }, 0);
+
     return position;
 }
