d1d68250d01160b710a234633920bfa5ce29e3da
Elin Fritiofsson <fritiofsson@gfai.de>
2021-12-02 08:04:24 +0100

    Cleaned up

--- day1/solve.test.js
+++ day1/solve.test.js
@@ -2,20 +2,20 @@ const solve = require('./solve');
 let dataFile = "testdata.txt";
 let dataFile2 = "testdata2.txt";
 
-test('countIncreases counts number of increases in data series', async () => {
+test('countIncreases counts number of increases correctly in first test data', async () => {
 
     let dataArray = await solve.readData(dataFile); 
     expect(solve.countIncreases(dataArray)).toBe(7);
 });
 
-test('countIncreases counts number of increases in data series', async () => {
+test('countIncreases counts number of increases correctly in second test data', async () => {
 
     let dataArray = await solve.readData(dataFile2); 
     expect(solve.countIncreases(dataArray)).toBe(2);
 });
 
 
-test('readFile creates an array with 10 entries from the testfile', async () => {
+test('readFile creates an array with 10 entries from the first testfile', async () => {
 
     let solutionArray = await solve.readData(dataFile);
     expect(solutionArray.length).toBe(10);
